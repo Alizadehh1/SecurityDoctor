589e0a086131bba87c5211d56365f733ec737653
sahibtt <alisal@code.edu.az>
2024-02-25 20:10:52 +0400

    created

---
+++ SecurityDoctor.WebUI/SecurityDoctor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SecurityDoctor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
